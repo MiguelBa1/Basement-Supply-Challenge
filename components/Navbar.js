8b5a0228d22f1aa634cc3804e39302fea10d7ce6
Miguel Barrera <59462086+MiguelBa1@users.noreply.github.com>
2022-03-18 23:20:13 -0500

    Media queries agregados con imagenes cambiantes

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,22 @@
 import Image from "next/image";
 
+import miniLogo from "../public/mini-logo.svg";
 import logo from "../public/logo.svg";
 import hd_4k from "../public/hd-4k.svg";
 
 const Nabvar = () => {
     return (
-        <nav className="flex justify-between p-5">
-            <Image alt="Basement" src={logo} />
-            <Image alt="Basement" src={hd_4k} />
-            <button>CART(<span>0</span>)</button>
+        <nav className="flex justify-between p-4 md:p-5">
+            <div className="hidden sm:inline">
+                <Image alt="Basement logo" src={logo} />
+            </div>
+            <div className="sm:hidden">
+                <Image alt="Basement mini logo" src={miniLogo} />
+            </div>
+            <div className="hidden md:inline">
+                <Image alt="Quality logos" className="hidden" src={hd_4k} />
+            </div>
+            <button className="border-2 rounded-3xl px-5">CART(<span id="items">0</span>)</button>
         </nav>
     )
 }
