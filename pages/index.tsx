f3e6b6f9a3fa83fb71463f2b7aa6c77f7618f395
Miguel Barrera <59462086+MiguelBa1@users.noreply.github.com>
2022-03-18 23:22:18 -0500

    Estructura basica de la pagina principal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,36 @@
-import type {NextPage} from "next";
-import Navbar from "../components/Navbar.js";
-
-
+import type { NextPage } from "next";
 import Image from "next/image";
+import Navbar from "../components/Navbar.js";
+import Marquee from "../components/Marquee.js";
+import Product from "../components/Product.js";
 
 import header from "../public/header.svg";
+import footer from "../public/footer.svg";
+import shirt from "../public/products/shirt.png"
+import hoodie from "../public/products/hoodie.png"
+import cap from "../public/products/cap.png"
 
 const Home: NextPage = () => {
   return (
     // <div className="h-full flex bg-black">
     <>
-      <header className="max-w-full">
+      <header >
         <Navbar />
-        <div className="w-screen relative">
-          <Image className="max-w-full" alt="Basement" src={header} />
+        <div className="flex justify-center my-2 mx-2">
+          <Image alt="Basement supply" src={header} />
         </div>
       </header>
+      <Marquee />
+      <div className="m-4 sm:flex gap-5">
+        <Product img={shirt} name="Black t-shirt" price={7.45} />
+        <Product img={hoodie} name="Black hoodie" price={13} />
+        <Product img={cap} name="Black cap" price={23} />
+      </div>
+      <footer>
+        <div className="flex justify-center my-3 mx-2">
+          <Image alt="Wear everyday" src={footer} />
+        </div>
+      </footer>
     </>
   );
 };
