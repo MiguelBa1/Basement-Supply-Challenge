9efa8274809198e6aec90770052eb5a8d63d33cf
Miguel Barrera <59462086+MiguelBa1@users.noreply.github.com>
2022-03-18 23:20:37 -0500

    Colores, animaciones, fuentes agregados

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        marquee: 'marquee 20s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+      colors: {
+        'initial-black': '#1D1D1D',
+        'final-gray': 'rgba(21, 21, 21, 0)'
+      },
+      fontFamily: {
+        'basement': ['Basement Grotesque']
+      }
+    },
   },
   variants: {
     extend: {},
