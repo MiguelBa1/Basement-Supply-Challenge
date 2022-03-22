c08bf7d3a2980235131966a361ba103ea4f6c294
Miguel Barrera <59462086+MiguelBa1@users.noreply.github.com>
2022-03-21 20:18:20 -0500

    Flex agregado para grandes pantallas

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,14 +14,14 @@ const Home: NextPage = () => {
   return (
     // <div className="h-full flex bg-black">
     <>
-      <header >
+      <header>
         <Navbar />
         <div className="flex justify-center my-2 mx-2">
           <Image alt="Basement supply" src={header} />
         </div>
       </header>
       <Marquee />
-      <div className="m-4 sm:flex gap-5">
+      <div className="m-4 sm:flex gap-5 justify-between">
         <Product img={shirt} name="Black t-shirt" price={7.45} />
         <Product img={hoodie} name="Black hoodie" price={13} />
         <Product img={cap} name="Black cap" price={23} />
