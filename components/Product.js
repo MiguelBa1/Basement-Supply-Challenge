e139c74a1eb80e1c8b2554f0511fe819b8948ca3
Miguel Barrera <59462086+MiguelBa1@users.noreply.github.com>
2022-03-21 20:16:50 -0500

    Imagen agregada en hover

--- components/Product.js
+++ components/Product.js
@@ -1,13 +1,20 @@
 import Image from "next/image";
 
+import hoverImg from "../public/add-to-cart.svg"
+
 const Product = ({img, name, price}) => {
     return (
         <div>
             {/* Background degradient */}
-            <div className="bg-gradient-to-b from-initial-black to-final-gray border-b-2"> 
-                <Image className="h-50" src={img}/>
+            <div className="bg-gradient-to-b from-final-gray to-initial-black border-b-2 relative"> 
+                <div className="z-0">
+                    <Image src={img}/>
+                </div>
+                <div className="absolute inset-0 opacity-0 hover:opacity-100 duration-300 z-100 flex justify-center items-center">
+                    <Image src={hoverImg}/>
+                </div>
             </div>
-            <div className="flex justify-between font-basement">
+            <div className="flex justify-between text-xl">
                 <span>{name}</span>
                 <span>{`$ ${price}`}</span>
             </div>
