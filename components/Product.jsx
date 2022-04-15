7631111c2e4005ad375dcf4b846ae9f39d5a23e8
Miguel Barrera <mibarrera@unal.edu.co>
2022-04-15 15:49:02 -0500

    mock agregado y despliegue de productos con map

--- components/Product.jsx
+++ components/Product.jsx
@@ -8,7 +8,7 @@ const Product = ({img, name, price}) => {
             {/* Background degradient */}
             <div className="bg-gradient-to-b from-final-gray to-initial-black border-b-2 relative"> 
                 <div className="z-0">
-                    <Image src={img}/>
+                    <Image src={img} width={880} height={1156}/>
                 </div>
                 <div className="absolute inset-0 opacity-0 hover:opacity-100 duration-300 z-100 flex justify-center items-center">
                     <Image src={hoverImg}/>
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,13 +6,14 @@ import Product from "../components/Product.js";
 
 import header from "../public/header.svg";
 import footer from "../public/footer.svg";
-import shirt from "../public/products/shirt.png"
-import hoodie from "../public/products/hoodie.png"
-import cap from "../public/products/cap.png"
+import shirt from "../public/products/shirt.png";
+import hoodie from "../public/products/hoodie.png";
+import cap from "../public/products/cap.png";
+
+import products from "../product/mock.json";
 
 const Home: NextPage = () => {
   return (
-    // <div className="h-full flex bg-black">
     <>
       <header>
         <Navbar />
@@ -22,9 +23,12 @@ const Home: NextPage = () => {
       </header>
       <Marquee />
       <div className="m-4 sm:flex gap-5 justify-between">
-        <Product img={shirt} name="Black t-shirt" price={7.45} />
+        {products.map(prod =>
+          <Product img={prod.img} name={prod.name} price={prod.price} />
+        )}
+        {/* <Product img={shirt} name="Black t-shirt" price={7.45} />
         <Product img={hoodie} name="Black hoodie" price={13} />
-        <Product img={cap} name="Black cap" price={23} />
+        <Product img={cap} name="Black cap" price={23} /> */}
       </div>
       <footer>
         <div className="flex justify-center my-3 mx-2">
