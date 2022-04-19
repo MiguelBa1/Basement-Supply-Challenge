ec9505246db8e9ee475bf857f5d8372ebc43e771
Miguel Barrera <mibarrera@unal.edu.co>
2022-04-18 21:54:59 -0500

    Meta tags modificados

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,30 @@
-import type {AppProps} from "next/app";
+import type { AppProps } from "next/app";
 import Head from "next/head";
 
 import "../css/global.css";
 
-function App({Component, pageProps}: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        {/* <!-- Primary Meta Tags --> */}
         <title>Basement Supply</title>
-        <meta content="Coding challenge for basement.studio." name="description" />
+        <meta name="title" content="Basement Supply" />
+        <meta name="description" content="Coding challenge for basement.studio." />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://prueba-nextjs.miguelbarrera4.repl.co/" />
+        <meta property="og:title" content="Basement Supply" />
+        <meta property="og:description" content="Coding challenge for basement.studio." />
+        <meta property="og:image" content="https://prueba-nextjs.miguelbarrera4.repl.co/og.png" />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://prueba-nextjs.miguelbarrera4.repl.co/" />
+        <meta property="twitter:title" content="Basement Supply" />
+        <meta property="twitter:description" content="Coding challenge for basement.studio." />
+        <meta property="twitter:image" content="https://prueba-nextjs.miguelbarrera4.repl.co/og.png" />
       </Head>
       <Component {...pageProps} />
     </>
