2b828df6c76932e10543f6763f98b994d9e9bc2a
Miguel Barrera <mibarrera@unal.edu.co>
2022-04-18 21:55:23 -0500

    types para el reducer

---
+++ context/Types.js
@@ -0,0 +1,5 @@
+export const SHOW_HIDE_CART = "SHOW_HIDE_CART";
+export const ADD_TO_CART = "ADD_TO_CART";
+export const REMOVE_ITEM = "REMOVE_ITEM";
+export const CHANGE_CART_QTY = "CHANGE_CART_QTY";
+export const CHANGE_CART_SIZE = "CHANGE_CART_SIZE";
