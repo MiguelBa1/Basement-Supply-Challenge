093a7096f97560c34b3f82f80a9ddecc45efcaac
Miguel Barrera <miguelbarreraa05@gmail.com>
2023-08-13 10:11:33 -0500

    Add localStorage to maanage the cart information

--- context/cart/CartState.js
+++ context/cart/CartState.js
@@ -1,4 +1,4 @@
-import {useReducer, useMemo} from "react";
+import {useReducer, useMemo, useEffect} from "react";
 
 import {
   SHOW_HIDE_CART,
@@ -12,12 +12,12 @@ import CartContext from "./CartContext";
 import CartReducer from "./CartReducer";
 
 const CartState = ({children}) => {
-  const initalState = {
+  const initialState = {
     showCart: false,
     cartItems: [],
   };
 
-  const [state, dispatch] = useReducer(CartReducer, initalState);
+  const [state, dispatch] = useReducer(CartReducer, initialState);
 
   const addToCart = (item) => {
     dispatch({type: ADD_TO_CART, payload: {...item, quantity: 1}});
@@ -50,6 +50,16 @@ const CartState = ({children}) => {
       changeSize,
     };
   }, [state.showCart, state.cartItems]);
+
+  useEffect(() => {
+    const cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+
+    dispatch({type: "LOAD_CART_FROM_STORAGE", payload: cartItems});
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+  }, [state.cartItems]);
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
