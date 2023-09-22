a451beed87d4c57c3dcaa42fba665c4fb5a03c5e
Miguel Barrera <miguelbarreraa05@gmail.com>
2023-09-22 15:44:46 -0500

    Add transition animation for CartModal

--- components/CartModal.tsx
+++ components/CartModal.tsx
@@ -30,49 +30,52 @@ const Cart = () => {
 
   return (
     <>
-      {showCart && (
+      <div
+        className={`fixed inset-0 z-50 flex items-center justify-center transition-opacity duration-500 bg-black bg-opacity-50 ${
+          showCart ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
+        }`}
+        onClick={handleBackgroundClick}
+      >
         <div
-          className="fixed inset-0 z-50 flex items-center justify-center transition-all duration-500 bg-black bg-opacity-50"
-          onClick={handleBackgroundClick}
+          className={`top-0 right-0 grid h-screen gap-2 p-5 overflow-auto bg-black font-basement-black md:h-3/4 md:border grid-rows-cart md:fixed transform transition-transform duration-500 ${
+            showCart ? "translate-x-0" : "translate-x-full"
+          }`}
         >
-          <div className="top-0 right-0 grid h-screen gap-2 p-5 overflow-auto bg-black font-basement-black md:h-3/4 md:border grid-rows-cart md:fixed">
-            <div className="text-right">
-              <button onClick={showHideCart}>→ CLOSE</button>
-            </div>
-            <div className="text-center text-8xl">
-              YOUR{" "}
-              <span className="text-stroke-white text-stroke-2 text-fill-transparent">CART</span>
-            </div>
-            <div>
-              {cartItems.length === 0 ? (
-                <h4>Cart is Empty</h4>
-              ) : (
-                <ul>
-                  {cartItems.map((item) => (
-                    <CartItem key={item._id} item={item} />
-                  ))}
-                </ul>
-              )}
-            </div>
-            <div className="md:grid grid-cols-bottom-cart">
-              <div className="flex justify-between gap-2 md:justify-start md:text-3xl">
-                <div>TOTAL:</div>
-                <div>
-                  {formatCurrency(
-                    cartItems.reduce((amount, item) => item.price * item.qty + amount, 0),
-                  )}
-                </div>
+          <div className="text-right">
+            <button onClick={showHideCart}>→ CLOSE</button>
+          </div>
+          <div className="text-center text-8xl">
+            YOUR <span className="text-stroke-white text-stroke-2 text-fill-transparent">CART</span>
+          </div>
+          <div>
+            {cartItems.length === 0 ? (
+              <h4>Cart is Empty</h4>
+            ) : (
+              <ul>
+                {cartItems.map((item) => (
+                  <CartItem key={item._id} item={item} />
+                ))}
+              </ul>
+            )}
+          </div>
+          <div className="md:grid grid-cols-bottom-cart">
+            <div className="flex justify-between gap-2 md:justify-start md:text-3xl">
+              <div>TOTAL:</div>
+              <div>
+                {formatCurrency(
+                  cartItems.reduce((amount, item) => item.price * item.qty + amount, 0),
+                )}
               </div>
-              <button
-                className="pt-4 text-5xl text-center text-black transition-all border-t duration-50 md:pt-0 md:border-0 md:text-3xl text-stroke-white text-stroke-2 hover:text-white hover:text-stroke-none"
-                onClick={handleCheckout}
-              >
-                CHECKOUT
-              </button>
             </div>
+            <button
+              className="pt-4 text-5xl text-center text-black transition-all border-t duration-50 md:pt-0 md:border-0 md:text-3xl text-stroke-white text-stroke-2 hover:text-white hover:text-stroke-none"
+              onClick={handleCheckout}
+            >
+              CHECKOUT
+            </button>
           </div>
         </div>
-      )}
+      </div>
     </>
   );
 };
