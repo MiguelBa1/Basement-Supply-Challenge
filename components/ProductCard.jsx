b23ac1073b326e27e8895d98dd0e31920b7738b7
Miguel Barrera <miguelbarreraa05@gmail.com>
2023-09-22 16:06:07 -0500

    Add hover animation to Product Card

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -6,10 +6,11 @@ import hoverImg from "../public/add-to-cart.svg";
 import CartContext from "../context/cart/CartContext";
 
 const ProductCard = ({_id, name, img, price}) => {
-  const {addToCart} = useContext(CartContext);
+  const {addToCart, showHideCart} = useContext(CartContext);
 
   const handleAddToCart = () => {
     addToCart({_id, name, img, price});
+    showHideCart();
   };
 
   const formattedPrice = formatCurrency(price);
@@ -18,13 +19,13 @@ const ProductCard = ({_id, name, img, price}) => {
     <div>
       <button
         aria-label={`Add ${name} to cart`}
-        className="relative border-b-2 border-white bg-gradient-to-b from-final-gray to-initial-black"
+        className="relative transition-colors duration-500 border-b-2 border-white bg-gradient-to-b from-final-gray to-initial-black hover:from-black hover:to-gray-900"
         onClick={handleAddToCart}
       >
         <div>
           <Image alt={name} height={1156} src={img} width={880} />
         </div>
-        <div className="absolute inset-0 flex items-center justify-center duration-300 opacity-0 cursor-pointer hover:opacity-100">
+        <div className="absolute inset-0 flex items-center justify-center duration-500 opacity-0 cursor-pointer hover:opacity-100">
           <Image alt="hover image" src={hoverImg} />
         </div>
       </button>
